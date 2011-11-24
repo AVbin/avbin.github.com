e8acb2295aece8a32a7a206b51265a01978ce310
Nathan Stocks <nathan.stocks@gmail.com>
2011-11-24 16:06:56 -0700

    Renamed the Documentation page to Development and removed the search bar that doesn't function unless you're publishing on Mobile Me (or iCloud?)

--- AVbin/Documentation_files/Documentation.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20111124
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Documentation_files/DocumentationMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
