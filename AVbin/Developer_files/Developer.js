c994fe901565a022427665a90fc480c4d1de189a
Nathan Stocks <nathan.stocks@gmail.com>
2011-12-10 18:08:17 -0700

    Updating the web site.  Version 8 released across all platforms.  Unfortunately, probably lots of problems with it.  But at least something is built.

--- AVbin/Developer_files/Developer.js
+++ AVbin/Developer_files/Developer.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20111124
+// Created by iWeb 3.0.4 local-build-20111210
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Developers_files/DevelopersMoz.css')
+{loadMozillaCSS('Developer_files/DeveloperMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
--- AVbin/Download_files/Download.js
+++ AVbin/Download_files/Download.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20111128
+// Created by iWeb 3.0.4 local-build-20111210
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Download_files/DownloadMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
