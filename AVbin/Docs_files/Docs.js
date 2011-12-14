ae754420a30619d65da8a3d294203969a41a59fd
Nathan Stocks <nathan.stocks@gmail.com>
2011-12-13 23:35:03 -0700

    Added a Docs page that points to the Doxygen docs

---
+++ AVbin/Docs_files/Docs.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20111213
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Docs_files/DocsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
