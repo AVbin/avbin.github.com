1f82ac5750135a81fb5f980a16e2e0edcfe028e4
Nathan Stocks <nathan.stocks@gmail.com>
2012-06-18 12:35:16 -0600

    Put up the 8.1 release announcement

---
+++ AVbin/Home/Entries/2012/6/18_AVbin_8.1_Released_files/18_AVbin_8.1_Released.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20120618
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('18_AVbin_8.1_Released_files/18_AVbin_8.1_ReleasedMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
