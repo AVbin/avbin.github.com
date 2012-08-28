d1fda41e08fc41b3174417183a3f8f3d47ba13af
Nathan Stocks <nathan.stocks@gmail.com>
2012-08-28 09:52:36 -0600

    Updated the downloads page with info about the recent installer changes

--- AVbin/Download_files/Download.js
+++ AVbin/Download_files/Download.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20120823
+// Created by iWeb 3.0.4 local-build-20120828
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Download_files/DownloadMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
